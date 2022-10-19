1065b2822cf925b799a558f06632534bab294257
Catalin Ristache <Catalin_Ristache@waters.com>
2022-10-19 18:37:10 +0300

    Add initial dependencies

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,5 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+//add the endpoint for calling the service
+// first thing is the route
+// than the parameters that we're going to pass => 
+// the delagate method
+
+
+app.MapGet("observation/{zip}", (string zip, [FromQuery] int? days) =>
+{
+    //this is actually the delegate method that will call into the precipitation service business logic
+
+    //this is kind of a unit test that the service is responding
+    //return Results.Ok(zip);
+});
 
 app.Run();
