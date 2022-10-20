ec1a6b209399eab8c8819010ffedbe88680df142
Catalin Ristache <Catalin_Ristache@waters.com>
2022-10-20 15:43:50 +0300

    Add data access for precipitation service

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess
+{
+    //DbContext is an abstraction of the database
+    public class PrecipDbContext : DbContext
+    {
+        public PrecipDbContext() { }
+        public PrecipDbContext(DbContextOptions opts) : base(opts) { }
+
+        public DbSet<Precipitation> Precipitation { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) { 
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentityTableNames(modelBuilder);
+        }
+
+        private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Precipitation>(b => { b.ToTable("precipitation"); });
+        }
+    }
+}
--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,8 +1,19 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+// this MUST come BEFORE builder.Build()
+builder.Services.AddDbContext<PrecipDbContext>(
+    opts => {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
 
 var app = builder.Build();
 
@@ -12,12 +23,25 @@ var app = builder.Build();
 // the delagate method
 
 
-app.MapGet("observation/{zip}", (string zip, [FromQuery] int? days) =>
+app.MapGet("observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDbContext db) =>
 {
     //this is actually the delegate method that will call into the precipitation service business logic
 
     //this is kind of a unit test that the service is responding
     //return Results.Ok(zip);
+
+    //validate input
+    if (days == null || days < 1 || days > 30) {
+        return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+    }
+
+    var StartDate = DateTime.UtcNow -TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation
+        .Where(precip => precip.ZipCode == zip && precip.CreatedOn > StartDate)
+        .ToListAsync();
+
+    return Results.Ok(results);
+
 });
 
 app.Run();
