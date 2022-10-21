ff06945a706ad083f98f442d0d19ae5997fc5a29
Catalin Ristache <Catalin_Ristache@waters.com>
2022-10-21 15:14:09 +0300

    Corrections on dockerfiles and scripts to push to ECR

--- CloudWeather.Precipitation/build_and_push_ecr_image.sh
+++ CloudWeather.Precipitation/build_and_push_ecr_image.sh
@@ -2,7 +2,7 @@
 set -e
 
 aws ecr get-login-password --region us-east-1 --profile weather-ecr-agent | docker login --username AWS --password-stdin 021596293162.dkr.ecr.us-east-1.amazonaws.com
-docker build -f ./Dockerfile -t cloud-weather-temperature:latest
+docker build -f ./Dockerfile -t cloud-weather-temperature:latest .
 docker tag cloud-weather-temperature:latest 021596293162.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-temperature:latest
 docker push 021596293162.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-temperature:latest
 
--- CloudWeather.Temperature/build_and_push_ecr_image.sh
+++ CloudWeather.Temperature/build_and_push_ecr_image.sh
@@ -2,7 +2,7 @@
 set -e
 
 aws ecr get-login-password --region us-east-1 --profile weather-ecr-agent | docker login --username AWS --password-stdin 021596293162.dkr.ecr.us-east-1.amazonaws.com
-docker build -f ./Dockerfile -t cloud-weather-data-loader:latest
+docker build -f ./Dockerfile -t cloud-weather-data-loader:latest .
 docker tag cloud-weather-data-loader:latest 021596293162.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-data-loader:latest
 docker push 021596293162.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-data-loader:latest
 
